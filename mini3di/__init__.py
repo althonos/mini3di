0274c011cb7f31df091a135b7eccb534b417da8b
Martin Larralde <martin.larralde@embl.de>
2023-11-25 21:39:57 +0100

    Import `FeatureEncoder` in main project namespace

--- mini3di/__init__.py
+++ mini3di/__init__.py
@@ -1,10 +1,10 @@
 """A NumPy port of the ``foldseek`` code for encoding structures to 3di.
 """
 
-__all__ = ["Encoder"]
+__all__ = ["Encoder", "FeatureEncoder"]
 __version__ = "0.1.0"
 __author__ = "Martin Larralde <martin.larralde@embl.de>"
 __license__ = "GPLv3"
 __credits__ = "Martin Steinegger and his lab for ``foldseek``"
 
-from .encoder import Encoder
+from .encoder import Encoder, FeatureEncoder
